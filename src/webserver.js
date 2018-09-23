3a707331ac85d2537dda41668f52ee1c4818d01e
Pedro Javier Nicolás Zamora <pedrojavier.nz@gmail.com>
2018-09-23 23:43:16 +0200

    Remove parcel

--- src/webserver.js
+++ src/webserver.js
@@ -17,7 +17,7 @@ app.use(basicAuth({
   challenge: true,
 }));
 
-app.use(express.static('dist'));
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
