0a16b7218f7155053103d8b2ceab998e2a42a1ad
Pedro Javier Nicolás Zamora <pedrojavier.nz@gmail.com>
2018-09-30 16:33:37 +0000

    Fix some stuff

--- src/sensor.py
+++ src/sensor.py
@@ -3,8 +3,10 @@ import requests
 from time import sleep
 
 req_url = 'http://localhost:7777/'
+req_headers= {'content-type': 'application/json'}
 sensor = Adafruit_DHT.DHT22
 pin = 4
+delay_time = 5
 
 def createRequestData(temperature, humidity, electricalOutlet):
     json = '''{{ "temperature": {}, "humidity": {}, "electricalOutlet": {} }}'''
@@ -12,15 +14,22 @@ def createRequestData(temperature, humidity, electricalOutlet):
 
 def sendData(temperature, humidity, electricalOutlet):
     data = createRequestData(temperature, humidity, electricalOutlet)
-    response = requests.post(req_url, data=data)
-    # TODO Get the response status
+    print(data)
+    try:
+        # TODO Get the response status
+        response = requests.post(req_url, data=data, headers=req_headers)
+    except:
+        print("Error connecting to TriggerServer")
 
 def measureData():
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    humidity = round(humidity, 2)
+    temperature = round(temperature, 2)
     # TODO Get electricalOutlet
-    print(humidity, temperature)
+    electricalOutlet = "true"
+    print(str(humidity) + " %,  " + str(temperature) + " C")
     sendData(temperature, humidity, electricalOutlet)
 
 while True:
     measureData()
-    sleep(10)
+    sleep(delay_time)
